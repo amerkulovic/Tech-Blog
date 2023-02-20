13208e28caea0231157111a0e72ceef6280d8f34
Amer Kulovic <amer.kulovic@yahoo.com>
2023-02-20 13:40:14 -0600

    created the single page post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -70,4 +70,27 @@ router.get("/dashboard", withAuth, async (req, res) => {
   res.render("dashboard", { posts, loggedIn: req.session.loggedIn });
 });
 
+router.get("/post", withAuth, async (req, res) => {
+  const postData = await Post.findAll({
+    where: {
+      user_id: req.session.userId,
+    },
+  });
+  const posts = postData.map((post) => post.get({ plain: true }));
+  res.render("post", { posts, loggedIn: req.session.loggedIn });
+});
+
+router.get("/post/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {});
+    const post = postData.get({ plain: true });
+    res.render("post", {
+      ...post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
